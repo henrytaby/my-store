d9417759395098c67a610a147151b82cdd1974d6
Henry Taby <henry.taby@gmail.com>
2022-06-07 10:59:08 -0400

    [my-store] componentes iniciales nvmrc para anunciar la versión en la que se esta desarrollando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-store';
+  name = 'Henry Taby';
+  age = 42;
+  img = "https://pbs.twimg.com/profile_images/505039074529275907/r3aPsQOK_400x400.png";
+  btnDisabled = true;
+  person = {
+    name: 'Henry Taby',
+    age: 42,
+    avatar: 'https://pbs.twimg.com/profile_images/505039074529275907/r3aPsQOK_400x400.png'
+  };
+
+
+  toglleButton(){
+    this.btnDisabled  = !this.btnDisabled;
+  };
+
+  increaseAge(){
+    this.person.age +=1;
+
+  }
 }
