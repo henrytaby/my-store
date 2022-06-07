45ade9d45d62a68c6ef5fd2786b79d16d0b0accf
Henry Taby <henry.taby@gmail.com>
2022-06-07 16:11:48 -0400

    [Otros eventos que puedes escuchar]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,4 +26,12 @@ export class AppComponent {
     this.person.age +=1;
 
   }
+  onScroll(event: Event){
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop);
+  }
+  changeName(event: Event){
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
+  }
 }
